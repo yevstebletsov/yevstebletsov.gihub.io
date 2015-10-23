2725e0e9d461cc448fa19af04ba11b26a4400423
Stebletsov.Yevgeniy <yevgeniy.stebletsov@gameloft.com>
2015-10-23 18:43:30 +0300

    - work

---
+++ app/controller/page/Test.js
@@ -0,0 +1,22 @@
+/**
+ * @class App.controller.page.Test
+ * @extend Ext.app.Controller
+ *
+ * Controllers which handle logic general logic for page
+ *
+ * @author Yevgeniy Stebletsov <yevgeniy.stebletsov@gmail.com>
+ */
+
+Ext.define('App.controller.page.Test', {
+    extend: 'Ext.app.Controller',
+
+    views: [
+        'viewport.Test'
+    ],
+
+    initProcess: true,
+    
+    init: function() {
+        "use strict";
+    }
+});
