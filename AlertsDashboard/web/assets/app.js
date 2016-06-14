4f8d89e1cdf16d799853657d8b53847fa072c67e
Stebletsov.Yevgeniy <yevgeniy.stebletsov@gameloft.com>
2016-06-14 15:01:20 +0300

    - work

--- AlertsDashboard/web/assets/app.js
+++ AlertsDashboard/web/assets/app.js
@@ -7,6 +7,10 @@
 /**
  * Created by yevgeniy.stebletsov on 14.06.2016.
  */
+
+/**
+ * Created by yevgeniy.stebletsov on 14.06.2016.
+ */
 (function(){
     angular.module('filter', []);
 })();
@@ -19,7 +23,20 @@
     angular.module('app')
         .controller('IndexController', function($scope)
         {
-           
+            var show = true;
+            var showdash = false;
+            $scope.show = show;
+            $scope.showdash = showdash;
+
+            $scope.alerts = function(){
+                $scope.show = true;
+                $scope.showdash = false;
+            }
+
+            $scope.dashboard = function(){
+                $scope.show = false;
+                $scope.showdash = true;
+            }
         })
     ;
 })();
